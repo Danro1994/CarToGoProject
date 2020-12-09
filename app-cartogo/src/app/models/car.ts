858fd8f00fe6e0dc390b8954ff1e523e3b6ec104
wendyny <wenyen9306@gmail.com>
2020-12-09 02:24:57 -0600

    actualizando modelo Car

--- app-cartogo/src/app/models/car.ts
+++ app-cartogo/src/app/models/car.ts
@@ -1,4 +1,31 @@
+import { Condition } from './condition';
+import { Model } from './model';
+import { Photo } from './photo';
+import { Seller } from './seller';
+import { Status } from './status';
+
 export class Car{
     Id: Number;
-    Name: String;
+    ModelId:Number ;
+    Model: Model ;
+    PhotoId:Number ;
+    Photo: Photo ;
+    Year:Number ;
+    Color: String ;
+    Mileage:Number ;
+    Fuel: String ;
+    Engine: String ;
+    Transmission: String ;
+    Traction: String ;
+    Seats: String;
+    AirConditione: boolean ;
+    Windows : String;
+    StatusId:Number ;
+    Status :Status ;
+    SellerId :Number;
+    Seller :Seller ;
+    ConditionId :Number;
+    Condition :Condition ;
+    Price :Number;
+    Comments: String ;
 }
