421064fd183e58a3131eebb66b7c99986c5c8e63
Kevin Madrid <kjmadrid@unitec.edu>
2020-12-10 14:05:39 -0600

    kevin

--- app-cartogo/src/app/carro-usado/carro-usado.module.ts
+++ app-cartogo/src/app/carro-usado/carro-usado.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CarroUsadoPageRoutingModule } from './carro-usado-routing.module';
 
 import { CarroUsadoPage } from './carro-usado.page';
+
+
 
 @NgModule({
   imports: [
